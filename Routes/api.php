8c98164f9245fba062db3463aab0303acf4dc8c3
luoyueliang <luoyueliang@gmail.com>
2026-07-14 10:27:05 +0800

    fix: resolve SPA API P1 issues
    RBAC Middleware:
    - User api.php: tenant.*, member.*, setting.* permissions
    - Payment api.php: payment.view, payment.create
    - Storage api.php: file.upload, file.delete
    
    File Security:
    - Added mime type validation (images, documents, archives only)
    - Fixed Content-Disposition header injection
    - Added module parameter validation with whitelist
    - Added per_page cap to adminIndex

--- Routes/api.php
+++ Routes/api.php
@@ -5,19 +5,46 @@ use MultiTenantSaas\Modules\User\Http\Controllers\TenantController;
 use MultiTenantSaas\Modules\User\Http\Controllers\TenantMemberController;
 use MultiTenantSaas\Modules\User\Http\Controllers\TenantSettingController;
 
-Route::get('/tenants', [TenantController::class, 'index']);
-Route::post('/tenants', [TenantController::class, 'store']);
-Route::get('/tenants/{tenantId}', [TenantController::class, 'show']);
-Route::put('/tenants/{tenantId}', [TenantController::class, 'update']);
-Route::delete('/tenants/{tenantId}', [TenantController::class, 'destroy']);
-Route::post('/tenants/{tenantId}/suspend', [TenantController::class, 'suspend']);
-Route::post('/tenants/{tenantId}/activate', [TenantController::class, 'activate']);
+Route::middleware('rbac.permission:tenant.view')->group(function () {
+    Route::get('/tenants', [TenantController::class, 'index']);
+    Route::get('/tenants/{tenantId}', [TenantController::class, 'show']);
+});
 
-Route::get('/tenants/{tenantId}/members', [TenantMemberController::class, 'index']);
-Route::post('/tenants/{tenantId}/members', [TenantMemberController::class, 'store']);
-Route::put('/tenants/{tenantId}/members/{userId}', [TenantMemberController::class, 'update']);
-Route::delete('/tenants/{tenantId}/members/{userId}', [TenantMemberController::class, 'destroy']);
+Route::middleware('rbac.permission:tenant.create')->group(function () {
+    Route::post('/tenants', [TenantController::class, 'store']);
+});
 
-Route::get('/tenants/{tenantId}/settings/{group?}', [TenantSettingController::class, 'index']);
-Route::put('/tenants/{tenantId}/settings/{group}', [TenantSettingController::class, 'update']);
-Route::post('/tenants/{tenantId}/settings/sms/test', [TenantSettingController::class, 'testSms']);
+Route::middleware('rbac.permission:tenant.update')->group(function () {
+    Route::put('/tenants/{tenantId}', [TenantController::class, 'update']);
+    Route::post('/tenants/{tenantId}/suspend', [TenantController::class, 'suspend']);
+    Route::post('/tenants/{tenantId}/activate', [TenantController::class, 'activate']);
+});
+
+Route::middleware('rbac.permission:tenant.delete')->group(function () {
+    Route::delete('/tenants/{tenantId}', [TenantController::class, 'destroy']);
+});
+
+Route::middleware('rbac.permission:member.view')->group(function () {
+    Route::get('/tenants/{tenantId}/members', [TenantMemberController::class, 'index']);
+});
+
+Route::middleware('rbac.permission:member.create')->group(function () {
+    Route::post('/tenants/{tenantId}/members', [TenantMemberController::class, 'store']);
+});
+
+Route::middleware('rbac.permission:member.update')->group(function () {
+    Route::put('/tenants/{tenantId}/members/{userId}', [TenantMemberController::class, 'update']);
+});
+
+Route::middleware('rbac.permission:member.delete')->group(function () {
+    Route::delete('/tenants/{tenantId}/members/{userId}', [TenantMemberController::class, 'destroy']);
+});
+
+Route::middleware('rbac.permission:setting.view')->group(function () {
+    Route::get('/tenants/{tenantId}/settings/{group?}', [TenantSettingController::class, 'index']);
+});
+
+Route::middleware('rbac.permission:setting.update')->group(function () {
+    Route::put('/tenants/{tenantId}/settings/{group}', [TenantSettingController::class, 'update']);
+    Route::post('/tenants/{tenantId}/settings/sms/test', [TenantSettingController::class, 'testSms']);
+});
